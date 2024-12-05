c3936650380fd02fbf0d1b2e4ed6bc6b9c8f4b05
meenmin2123 <min1016alsrud@gmail.com>
2024-12-06 01:09:15 +0900

    create front project

---
+++ project/traffic/src/login/login.js
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react'
+
+export class 
+
+
+
+
+
+extends PureComponent {
+  render() {
+    return (
+      <div>
+        
+
+
+
+
+        
+      </div>
+    )
+  }
+}
+
+export default 
+
+
+
+
+
+
